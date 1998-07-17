4e6c348facb87537a03554d86ea8bf4dcb4d87b8
mt <>
1998-07-17 20:53:51 +0000

    preparing for dupe-checking

---
+++ h/dupe.h
@@ -0,0 +1,45 @@
+#ifndef DUPE_H
+#define DUPE_H
+#include <typesize.h>
+#include <fidoconfig.h>
+
+/* This header file contains the structures of the dupe file */
+
+#define DUPE_MAJOR_VER 0
+#define DUPE_MINOR_VER 1
+
+struct dupeFileHeader {
+   UINT16  fileHeaderSize;
+   UCHAR   majorVer, minorVer;
+   UINT32  dupePackHeaderSize, noOfPacks;
+};
+
+typedef struct dupeFileHeader s_dupeFileHeader;
+
+struct dupePackHeader {
+   UINT16  entrySize;
+   UINT32  noOfEntries;
+   UCHAR   time_tSize;
+   time_t  packTime;
+};
+
+typedef struct dupePackHeader s_dupePackHeader;
+
+struct dupeEntry {
+   UINT32 hash;
+};
+
+typedef struct dupeEntry s_dupeEntry;
+
+struct dupeMemory {
+   UINT16 entrySize;
+   UINT32 noOfEntries;
+   s_dupeEntry *entries;
+};
+
+typedef struct dupeMemory s_dupeMemory;
+
+s_dupeMemory *readDupeFile(s_area *area);
+int writeToDupeFile(s_area *area, s_dupeMemory newDupeEntries);
+
+#endif
