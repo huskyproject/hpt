c3a8d42305e8274c6ed2d136df98f9637a44cbb3
mt <>
1998-11-18 20:32:58 +0000

    added gpl header on areafix.c

--- src/areafix.c
+++ src/areafix.c
@@ -1,9 +1,31 @@
-/*
-  AreaFix by Max Levenkov
-  2:5000/117@fidonet
-  ml@nsk.uland.com
-*/
-
+/*:ts=8*/
+/*****************************************************************************
+ * AreaFix for HPT (FTN NetMail/EchoMail Tosser)
+ *****************************************************************************
+ * Copyright (C) 1998
+ *
+ * Max Levenkov
+ *
+ * Fido:     2:5000/117
+ * Internet: ml@online.nsk.su
+ * Novosibirsk, Russia
+ *
+ * This file is part of HPT.
+ *
+ * HPT is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2, or (at your option) any
+ * later version.
+ *
+ * HPT is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with HPT; see the file COPYING.  If not, write to the Free
+ * Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *****************************************************************************/
 #include <fcommon.h>
 #include <global.h>
 #include <pkt.h>
