ddf559c59c7a6122aab19927d4410ac67eb11023
ml <>
2000-01-30 17:17:57 +0000

    *** empty log message ***

--- src/global.c
+++ src/global.c
@@ -35,7 +35,7 @@ s_fidoconfig *config=NULL;
 
 const int VER_MAJOR = 0;
 const int VER_MINOR = 9;
-const int VER_PATCH = 5;
+const int VER_PATCH = 6;
 char      versionStr[22];
 
 int       cmToss = 0;
