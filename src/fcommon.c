cceb77a9429ef773478666d52065aea094979363
ml <>
2000-05-02 15:16:42 +0000

    *** empty log message ***

--- src/fcommon.c
+++ src/fcommon.c
@@ -278,7 +278,7 @@ int createTempPktFileName(s_link *link)
     counter = 0;
 	do {
 		sprintf(pfileName, "%s%0x", tmpPFileName, counter);
-		writeLogEntry(hpt_log,'1',"%s", pfileName);
+//		writeLogEntry(hpt_log,'1',"%s", pfileName);
 		counter++;
 	} while ((fexist(pfileName) || fileNameAlreadyUsed(NULL, pfileName)) &&
 			 (counter <= 15));
