523f90264d1a6112bccb64040cdd90a87cb2f0bf
mhaase <>
2000-10-30 19:59:56 +0000

    * hpt (areafix): with %rescan command now also /R=xx and /R allowed   e.g.: '%rescan area /r=10' is the same as '%rescan area 10'         '%rescan area' is the same as '%rescan /r'          (Micha)

--- src/areafix.c
+++ src/areafix.c
@@ -1409,6 +1409,8 @@ char *rescan(s_link *link, s_message *msg, char *cmd) {
     countstr = line;
     while (*countstr && (!isspace(*countstr))) countstr++;
     while (*countstr && (*countstr == ' ' || *countstr == '\t')) countstr++;
+    if (!strncasecmp(buffer+x, "/R=",3)) countstr = countstr + 3;
+    if (!strncasecmp(buffer+x, "/R",2)) countstr = '99999'; /* rescan all mails */
 
     if (*countstr != 0)
       {
