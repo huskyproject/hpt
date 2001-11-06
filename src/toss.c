4e7421807d0dfa4f32e243ba565f9ea54974bcd9
ml <>
2001-11-06 07:57:35 +0000

    cygwin makefile

--- src/toss.c
+++ src/toss.c
@@ -40,6 +40,14 @@
 #endif
 #endif
 
+#if (defined(__EMX__) || defined(__MINGW32__)) && defined(__NT__)
+/* we can't include windows.h for several reasons ... */
+#ifdef __MINGW32__
+int __stdcall CharToOemA(char *, char *);
+#endif
+#define CharToOem CharToOemA
+#endif
+
 #include <sys/stat.h>
 #if !(defined(__TURBOC__) || (defined (_MSC_VER) && (_MSC_VER >= 1200)))
 #include <unistd.h>
