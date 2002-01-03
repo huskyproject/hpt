3463a8799ad5e3040ae1c29c9507cb8ec8880d18
ml <>
2002-01-03 13:17:17 +0000

    change version number

--- src/global.c
+++ src/global.c
@@ -38,12 +38,12 @@ int initSMAPI = -1;
 
 /* basic version number */
 const int   VER_MAJOR   = 1;
-const int   VER_MINOR   = 0;
+const int   VER_MINOR   = 1;
 const int   VER_PATCH   = 0;
 
 
 /* branch is "" for CVS current, "-stable" for the release candiate branch  */
-const char *VER_BRANCH  = "-stable";
+const char *VER_BRANCH  = "";
 
 /* The service version string is empty for the first release done from a    */
 /* release candidate branch.  If subsequent service releases are necessary, */
