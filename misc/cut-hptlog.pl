832b0a8a34f46e45da7c8ad7e1c120743a6e03f1
ml <>
2002-01-25 18:21:05 +0000

    new scripts

---
+++ misc/cut-hptlog.pl
@@ -0,0 +1,83 @@
+#!/usr/bin/perl
+
+# script for cutting a hpt's log-file
+#
+# (c)2002 sven(2:5030/1346)
+
+use Time::Local;
+
+
+$templog='/tmp/worklog';
+
+if ( $#ARGV < 1 )
+{   print "syntax is:    cut-hpt.pl d:\\fido\\logs\\hpt.log 14\n";
+    print "\n";
+    print "where first parametr is FULL path to hpt's log-file,\n";
+    print "and second - is a number of days ";
+    print "what information in log will be saved\n";
+    <>;
+    exit;
+};
+die "Logfile ($ARGV[0]) not found" if (!-f $ARGV[0]);
+
+$SAVEDAY = $ARGV[1];
+
+($x,$x,$x,$day,$mon,$year,$x,$x,$x)=localtime(time()-$SAVEDAY*24*60*60);
+$SAVE = timelocal(0,0,0,$day,$mon,$year);
+($x,$x,$x,$day,$mon,$year,$x,$x,$x)=localtime(time());
+$keepit = 0;
+
+
+open OLDLOG,$ARGV[0] || die "$ARGV[0] not found! :(";
+open NEWLOG,'>'.$templog || die "Cannot create temp file ($templog)! :(";
+
+while (<OLDLOG>)
+{
+    if (/\A----------\s+\S+\s+(\d+)\s+(\w+)\s+(\d+)/)
+   {
+    $lday=$1;
+    %monthnum = (
+    'Jan' => 0,
+    'Feb' => 1,
+    'Mar' => 2,
+    'Apr' => 3,
+    'May' => 4,
+    'Jun' => 5,
+    'Jul' => 6,
+    'Aug' => 7,
+    'Sep' => 8,
+    'Oct' => 9,
+    'Nov' => 10,
+    'Dec' => 11,);
+    $lmon=$monthnum{$2};
+    $lyear=$3+100;
+    $LDATE = timelocal(0,0,0,$lday,$lmon,$lyear);
+    if ($LDATE >= $SAVE) { print NEWLOG; $keepit = 1; } else {$keepit = 0};
+   }
+else { if ($keepit == 1) {print NEWLOG}};
+}
+
+%monthnum = (
+0  => 'Jan',
+1  => 'Feb',
+2  => 'Mar',
+3  => 'Apr',
+4  => 'May',
+5  => 'Jun',
+6  => 'Jul',
+7  => 'Aug',
+8  => 'Sep',
+9  => 'Oct',
+10 => 'Nov',
+11 => 'Dec',);
+
+print NEWLOG "\n";
+printf NEWLOG "---------- Число: %02d %s %02d Лог обрезан нахрен!\n",
+              $day,$monthnum{$mon},$year-100;
+print NEWLOG "\n";
+
+close OLDLOG;
+close NEWLOG;
+rename $templog, $ARGV[0];
+
+print "File: $ARGV[0] is succesfully cutted.\n";
---
+++ misc/pretty.pl
@@ -0,0 +1,231 @@
+#!/usr/bin/perl
+
+# :: Запустите этот скрипт с ключом '-h' для справки ::
+
+$stopat = 5;
+N: foreach (@ARGV) {
+    help() if (/^-h$/);
+    $df=$1, next N if (/^-desc(.+)/);
+    $ds=1,  next N if (/^-set$/);
+    $na=1,  next N if (/^-na$/);
+    $dl=1,  next N if (/^-d$/);
+    $bk=1,  next N if (/^-b$/);
+    $ns=1,  next N if (/^-ns$/);
+    $nf=1,  next N if (/^-nf$/);
+    $stopat=3, next N if (/^-nfa$/);
+    $no=1,  next N if (/^-no$/);
+    $nl=1,  next N if (/^-nl$/);
+    $alst=$_ if (-f $_);
+}
+help() unless ($alst);
+
+$aold='areas.old';
+$anew='areas.l$$';
+
+open LIST, "<$alst" or die "Error open $alst";
+open LOUT, ">$anew" or die "Error open temporary file";
+readna() if ($df);
+
+%echooptions = (
+'-lr' => '\d+',
+'-lw' => '\d+',
+'-p' => '\d+',
+'-mandatory' => '',
+'-ccoff' => '',
+'-$m' => '\d+',
+'-nopack' => '',
+'-killRead' => '',
+'-keepUnread' => '',
+'-a' => '\S+',
+'-b' => '\S+',
+'-g' => '\S+',
+'-keepsb' => '',
+'-tinysb' => '',
+'-killsb' => '',
+'-manual' => '',
+'-dosfile' => '',
+'-h' => '',
+'-d' => '\".+\"',
+'-nopause' => '',
+'-DupeCheck' => '(off|move|del)',
+'-DupeHistory' => '\d+',
+'-nolink' => '',
+'-debug' => '',
+'-sbadd' => '',
+'-sbign' => ''
+);
+
+print ":: Начинаем сортировку. Возможно это займет много времени.\n";
+my @ml;
+
+$ln=0;
+foreach $line (<LIST>) {
+   chomp $line;
+   if (($type,$name,$file,$rest) = $line=~/^(\w+)\s+(\S+)\s+(\S+)\s+(.+)/i) {
+      
+      $rest=~s/\"(.*)\"/do{$_=$1, $_=~tr# #\x01#, '"'.$_.'"'}/eg;
+
+      @res = (split /\s+/, $rest);
+      map {tr/\x01/ /} @res;
+      @opt = ();
+      @lnk = ();
+
+      for ($i=0, $rest=''; $i<=$#res; $i++){
+                $ss = $res[$i];
+                $ss=~s/\(.*\)//;
+                ($opti) = (grep (/^\Q$ss/i, keys %echooptions));
+                # check is not implemented yet
+                $eo = $echooptions{$opti};
+                if ($eo) {
+                    push @opt, "$res[$i] $res[++$i]";
+                } elsif ($opti) {
+                    push @opt, $res[$i];
+                } else {
+                    if ($res[$i]=~/-(def|r|w|mn)/) {
+                        $lnk[$#lnk].=' '.$res[$i];
+                    } else {
+                        push @lnk, $res[$i];
+                    }
+                }
+      }
+      # foreach (@opt) { print "$_\, " }; print "\n";
+
+      $desc = (grep /^-d /, @opt)[0];
+      @opt  = grep !/^-d /, @opt;
+      if ($df and (!$desc or $ds)) {
+         $ndesc = description(lc($name));
+         $desc = "-d \"". $ndesc ."\"" if ($ndesc);
+      }
+
+      if ($no) {$rest = join " " ,@opt} else
+               {$rest = join " " ,sort {$a cmp $b} @opt};
+      if ($nl) {$links= join " " ,@lnk} else
+               {$links= join " " ,sort {mysort()} @lnk};
+      sub mysort {
+          my ($i,$j);
+          $i=$a; $j=$b;
+          $i=~s/(\d+)/'0' x (5-length($1)) . $1/eg;
+          $j=~s/(\d+)/'0' x (5-length($1)) . $1/eg;
+          return $i cmp $j;
+      }
+
+      &max(1, $type);
+      &max(2, $name);
+      &max(3, $file);
+      &max(4, $rest);
+      &max(5, $desc);
+      $lines[$ln] = [ (1, $type, $name, $file, $rest, $desc, $links) ];
+   } else {
+      $lines[$ln] = [ (0, $line) ];
+   }
+   print ".";
+   $ln++;
+}
+print "\n";
+
+sub max() {
+        my ($i, $s);
+        ($i, $s)=@_;
+        if ($ml[$i]<length($s)) {
+            $ml[$i]=length($s);
+        }
+}
+
+if (!$ns) {
+print ":: Сортируем список арий.\n";
+CN: for ($i=0; $i<=$ln; $i++) {
+        if ($lines[$i][0]) {
+            for ($j=$i; $j<=$ln; $j++) {
+                 if (!$lines[$j][0]) {
+                     @part=@lines[$i..$j-1];
+                     @part = sort {${$a}[2] cmp ${$b}[2]} @part;
+                     @lines[$i..$j-1]=@part;
+                     $i=$j;
+                     next CN;
+                 }
+            }
+        }
+}
+}
+
+if (!$nf) { print ":: Выравниваем информацию.\n" } else
+          { print ":: Объединяем информацию.\n" }
+for ($i=0; $i<$ln; $i++) {
+      @al = @{$lines[$i]};
+      if ($al[0]) {
+          $al[3] = 'passthrough' if ($al[3]=~/passthrough/i);
+          if (!$nf) {
+               for ($j=2; $j<=$stopat; $j++) {
+                    $al[$j].=' ' x ( $ml[$j]-length($al[$j]) );
+               }
+          }
+          # no desc
+          if (($dl) and not ($al[5]=~/\w/) ) {
+              $line = join ' ', (@al) [1,2,3,4,6];
+          } else {
+              $line = join ' ', (@al) [1,2,3,4,5,6];
+          }
+          $line =~ s/\s+$//;
+      } else {
+          $line = $al[1];
+      }
+      print LOUT "$line\n";
+}
+
+close LIST;
+close LOUT;
+rename($alst,$aold) unless ($bk);
+rename($anew,$alst);
+print ":: Процесс завершен.\n";
+
+sub readna() {
+        open DESC, "<$df" or die "Error open $df";
+        print ":: Читаем файл описаний арий.\n";
+        foreach $line (<DESC>) {
+           chomp $line;
+           if ($na) {
+                ($name,$tmp) = $line=~/(\S+)[\s\"]+(.+)/;
+                $tmp=~s/[\s\"]+$//;
+                $descript{lc($name)}=$tmp;
+           } else {
+             if ($line=~/(hold|down|),/i) {
+                ($name,$tmp)=(split(/,/,$line))[1,2];
+                $descript{lc($name)}=$tmp;
+             }
+           }
+        }
+}
+sub description() {
+    ($_)=@_;
+    return $descript{$_} if ($descript{$_});
+    # :: Здесь вы можете проставить свою реакцию на название арии ::
+    # return 'Some CityCat echo...' if (/^ru\.list\.citycat/);
+    # return 'Some ExUSSR echo...'  if (/^su\./);
+    # return 'Some Russian echo...' if (/^ru\./);
+    # return 'Some private echo...' if (/^pvt\./);
+    return '';
+}
+
+sub help() {
+        print "Usage: pretty.pl [-d] [-b] [-ns] [-nf[a]] [-no] [-nl] <areafile>\n";
+        print "                 [-desc<echodesc> [-na] [-set]]\n\n";
+        print "::  Ключ -d позволяет выравнивать в одну и ту же колонку линков и\n";
+        print "::    дескрипшены. Если у вас мало дескрипшенов, но много линков\n";
+        print "::    (как у 2:5080/102 ;-), то воспользуйтесь данным ключом.\n";
+        print "::  Ключ -b запрещает делать backup.\n";
+        print "::  Ключ -ns запрещает сортировать арии.\n";
+        print "::    По умолчанию сортируются только арии, идущие друг за другом.\n";
+        print "::  Ключ -nf запрещает выравнивание (с -nfa не выравниваются опции/линки).\n";
+        print "::  Ключ -no запрещает сортировать опции.\n";
+        print "::  Ключ -nl запрещает сортировать линков.\n";
+        print "::  Ключ -desc позволяет добавлять описания арий из файла типа\n";
+        print "::    echo5020.lst. Если вдобавок установлена опция -na, то описания\n";
+        print "::    будут браться из файла стандартного для hpt формата.\n";
+        print "::    Можно в принудительном порядке проставлять описания, для этого\n";
+        print "::    используйте ключ -set.\n";
+        print "::  Example 0: pretty.pl areas.lst\n";
+        print "::  Example 1: pretty.pl -descD:\\files\\Xofcelist\\echo5020.lst\n";
+        print "::                       -set -no -nl -ns areas.lst\n";
+        print "::  Внимание! Данная версия тестовая. Будьте осторожны.\n";
+        exit;
+}
