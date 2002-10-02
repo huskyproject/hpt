558e6b623097ddba9895a8a62b9037a7517a3db5
Stas Degteff <stas_degteff@users.sourceforge.net>
2002-10-02 14:28:19 +0000

    Small perl script produce html-formatted output for postings into "Documentation" area of http://sourceforge.net/projects/husky

---
+++ doc/faqfmt.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl
+
+# This small perl script produce html-formatted output for postings into
+# "documentation" area of http://sourceforge.net/projects/husky
+#
+# (c) Stas Degteff 2:5080/102
+
+print "<pre>\n";
+while( <> ){
+ chomp;
+
+ if( /\[(\d+)\]/ ){ print "<a name=$1></a>"; }
+ s/^(.*\[\d+\].*)$/<h3>$1<\/h3>/;
+ s/^(A:.*)$/<font color=brown>$1<\/font>/;
+ s/ *\/-+\/ */<hr>/;
+ s/Q(\d+)(\. .*)$/<a href=\#$1>Q$1$2<\/a>/;
+
+ print "$_\n";
+}
+print "</pre>\n";
