89dd9a926db1983ead366eb7ad6338b60bbed6b4
Stas Degteff <stas_degteff@users.sourceforge.net>
2002-10-07 12:31:04 +0000

    Fix color for "Q:" strings; fix multi-line Q:

--- doc/faqfmt.pl
+++ doc/faqfmt.pl
@@ -11,7 +11,7 @@ while( <> ){
 
  if( /\[(\d+)\]/ ){ print "<a name=$1></a>"; }
  s/^(.*\[\d+\].*)$/<h3>$1<\/h3>/;
- s/^(Q: )/<font color=blue>$1/;
+ s/^(Q: .*)$/<font color=blue>$1</font>/;
  s/^(A:.*)$/<\/font><font color=brown>$1<\/font>/;
  s/ *\/-+\/ */<hr>/;
  s/Q(\d+)(\. .*)$/<a href=\#$1>Q$1$2<\/a>/;
