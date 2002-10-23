53eec962d2062002cff7bc1f79b9234da1e40776
Max Chernogor <mche@users.sourceforge.net>
2002-10-23 10:23:25 +0000

    clean type redefinition for win32 compilers

--- src/post.c
+++ src/post.c
@@ -66,9 +66,6 @@ tearline generation added
 
 #if (defined(__EMX__) || defined(__MINGW32__)) && defined(__NT__)
 /* we can't include windows.h for several reasons ... */
-#ifdef __MINGW32__
-int __stdcall CharToOemA(char *, char *);
-#endif
 #define CharToOem CharToOemA
 #endif
 
