56bdc25988f04188f127cc74dfd0fd338223dde9
Andrew Sagulin <andrews42@users.sourceforge.net>
2003-05-06 04:31:51 +0000

    perltossbad() fix $reason bug

--- src/perl.c
+++ src/perl.c
@@ -1959,6 +1959,7 @@ int perltossbad(s_message *msg, char *areaName, hs_addr pktOrigAddr, char *reaso
    time_t date;
    SV *svfromname, *svfromaddr, *svtoname, *svtoaddr, *svpktfrom;
    SV *svdate, *svtext, *svarea, *svsubj, *svret, *svchange, *svattr;
+   SV *svreason;
    STRLEN n_a;
    static int do_perltossbad=1;
    int pid, saveerr;
@@ -1982,6 +1983,7 @@ int perltossbad(s_message *msg, char *areaName, hs_addr pktOrigAddr, char *reaso
      svarea     = perl_get_sv("area",     TRUE);
      svtoaddr   = perl_get_sv("toaddr",   TRUE);
      svattr     = perl_get_sv("attr",     TRUE);
+     svreason   = perl_get_sv("reason",   TRUE);
      sv_setpv(svfromname, msg->fromUserName);
      sv_setpv(svfromaddr, aka2str(msg->origAddr));
      sv_setpv(svtoname,   msg->toUserName);
@@ -1995,6 +1997,7 @@ int perltossbad(s_message *msg, char *areaName, hs_addr pktOrigAddr, char *reaso
      sv_setpv(svpktfrom,  aka2str(pktOrigAddr));
      sv_setsv(svchange,   &sv_undef);
      sv_setuv(svattr,     msg->attributes | parse_flags(msg->text));
+     sv_setpv(svreason,   reason);
      if (areaName)
      { sv_setpv(svarea,   areaName);
        sv_setsv(svtoaddr, &sv_undef);
