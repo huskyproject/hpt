dd7ff2e2de344cac55bd5d10c5b5db78be7a7621
Stas Degteff <stas_degteff@users.sourceforge.net>
2006-10-06 12:05:42 +0000

    Fix Typo

--- src/toss.c
+++ src/toss.c
@@ -639,7 +639,7 @@ int putMsgInBadArea(s_message *msg, hs_addr pktOrigAddr, int writeAccess)
 	break;
     case 11:
 	reason = "length of CONFERENCE name is more than 60 symbols";
-	w_log(LL_ECHOMAIL, "Badmail reason: lenght of CONFERENCE name (areatag) is more than 60 symbols: '%s'", areaName);
+	w_log(LL_ECHOMAIL, "Badmail reason: length of CONFERENCE name (areatag) is more than 60 symbols: '%s'", areaName);
 	break;
     case 12:
 	reason = "Area killed (unsubscribed)";
